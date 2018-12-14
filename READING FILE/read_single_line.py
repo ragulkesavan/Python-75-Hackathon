5cb3d72bf086edd02e48a8ddf5e7d508b34affeb
ragulkesavan <35163962+ragulkesavan@users.noreply.github.com>
2018-12-15 00:51:34 +0530

    Add files via upload

---
+++ READING FILE/read_single_line.py
@@ -0,0 +1,24 @@
+#reading a single line from file
+#open the file
+f=open("girlslikeyou.txt","r")
+#readline() reads a single line from the file from where the read pointr is
+
+print f.readline()
+#reads 1st line
+print f.readline()
+#reads 2nd line
+print f.readline()
+#reads 3rd line
+print f.readline()
+#reads 4th line
+print f.readline()
+#reads 5th line
+
+'''
+OUTPUT:
+Spent 24 hours
+I need more hours with you
+You spent the weekend
+Getting even, ooh ooh
+We spent the late nights
+'''
