da34e0838e28dfd629e6df071cd399275e8c178a
ragulkesavan <35163962+ragulkesavan@users.noreply.github.com>
2018-12-15 01:23:22 +0530

    Update and rename write_a_mode.py to open_a_mode.py

---
+++ WRITE FILE/open_a_mode.py
@@ -0,0 +1,5 @@
+'''"a" mode will not delete all existing the contents in the file ,
+    if the file contains existing contents then the new entries are appened to the last of file 
+    and so all data are not lost and data are written as appended'''
+#opening in "w" mode
+f=open("lyrics.txt","a")
