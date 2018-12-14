1d5859b1523356bb196ec6f30f2e9d030938b40d
ragulkesavan <35163962+ragulkesavan@users.noreply.github.com>
2018-12-15 01:20:17 +0530

    Update and rename write.py to open_w_mode.py

---
+++ WRITE FILE/open_w_mode.py
@@ -0,0 +1,4 @@
+'''"w" mode will delete all the contents in the file if the file exists 
+    and so all data are lost and data are written from strating'''
+#opening in "w" mode
+f=open("lyrics.txt","w")
