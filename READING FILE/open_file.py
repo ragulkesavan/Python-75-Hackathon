4cf9eb257ef18b6a2b986faa817b04ce8547096a
ragulkesavan <35163962+ragulkesavan@users.noreply.github.com>
2018-12-15 00:14:48 +0530

    Update open_file.py

--- READING FILE/open_file.py
+++ READING FILE/open_file.py
@@ -1,1 +1,11 @@
-empty
+#OPENING A FILE FOR READING
+'''
+TO READ A FILE
+1.THE FILE SHOULD EXIST ON THE GIVEN PATH
+2.USE open() function
+3.FUNCTION TAKES TWO ARGUMENTS
+  1)FILE NAME ENCLOSED IN DOUBLE QUOTES LIKE girlslikeyou.txt
+  2)MODE -FOR READING USE "r" MODE
+'''
+f=open("girlslikeyou.txt","r")
+#file will be opened and pointed by f
