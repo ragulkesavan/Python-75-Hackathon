5024a7e7ca12509f26ca0ee6f3312b5e9d206060
ragulkesavan <35163962+ragulkesavan@users.noreply.github.com>
2018-12-15 00:51:01 +0530

    Add files via upload

---
+++ READING FILE/lines_to_list.py
@@ -0,0 +1,8 @@
+f=open("girlslikeyou.txt","r")
+print f.readlines
+'''
+OUTPUT:
+
+['Spent 24 hours\n', 'I need more hours with you\n', 'You spent the weekend\n', 'Getting even, ooh ooh\n', 'We spent the late nights\n', 'Making things right, between us\n', "But now it's all good baby\n", 'Roll that Backwood baby\n', 'And play me close\n', "'Cause girls like you\n", 'Run around with guys like me\n', "'Til sundown, when I come through\n", 'I need a girl like you, yeah yeah\n', 'Girls like you\n', 'Love fun, yeah me too\n', 'What I want when I come through\n', 'I need a girl like you, yeah yeah\n', 'Yeah yeah yeah\n', 'Yeah yeah yeah\n', 'I need a girl like you, yeah yeah\n', 'Yeah yeah yeah\n', 'Yeah yeah yeah\n', 'I need a girl like you, yeah yeah\n', 'I spent last night\n', 'On the last flight to you\n', 'Took a whole day up\n', 'Trying to get way up, ooh ooh\n', 'We spent the daylight\n', 'Trying to make things right between us\n', "And now it's all good baby\n", 'Roll that Backwood baby\n', 'And play\xe2\x80\xa6\n']
+''' 
+
