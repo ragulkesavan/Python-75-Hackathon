8c5673ed92b17c82b7c2d6aafb7e0d2ac4c31909
ragulkesavan <35163962+ragulkesavan@users.noreply.github.com>
2018-12-15 05:54:10 +0530

    Create add_method.py

---
+++ SET/add_method.py
@@ -0,0 +1,10 @@
+set1=set([1,2,3,"tallent","accurate"])
+print "normal set : ",set1
+set1.add("new")
+print "normal set after adding: ",set1
+
+'''
+OUTPUT:
+normal set :  set(['tallent', 1, 2, 3, 'accurate'])
+normal set after adding:  set([1, 2, 3, 'accurate', 'new', 'tallent'])
+'''
