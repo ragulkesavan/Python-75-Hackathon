2092455cb702f522610014096dbfe86a573645bc
ragulkesavan <35163962+ragulkesavan@users.noreply.github.com>
2018-12-15 11:45:44 +0530

    Create execution_python.py

---
+++ SUBPROCESS/execution_python.py
@@ -0,0 +1,18 @@
+subprocess.call(["ls"])
+subprocess.call(["python", "range_function.py"])
+
+
+
+'''
+OUTPUT:
+access_specifiers.py	   INHERITANCE	      READING FILE  TUPLES
+CLASS AND OBJECT	   LICENSE	      README.md     uncle_johny.py
+DICT			   LISTS	      RECURSION     VARIABLES
+FUNCTION		   random_numbers.py  SET	    WRITE FILE
+global vs local and scope  range_function.py  STRINGS
+0
+ENTER THE N VALUE : 8
+WHEN STEP=1 LIST =  [0, 1, 2, 3, 4, 5, 6, 7]
+WHEN STEP=2 LIST =  [0, 2, 4, 6]
+WHEN STEP=3 LIST =  [0, 3, 6]
+'''
