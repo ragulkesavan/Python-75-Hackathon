1aba03656415193753cb2549fe48e87c36e220ed
ragulkesavan <srk7official1998@gmail.com>
2018-12-15 18:42:51 +0530

    ragul

---
+++ LOOP/for_else.py
@@ -0,0 +1,17 @@
+#FOR WITH ELSE
+'''
+AS THERE IS NO SPECIFIC CONDITION IN FOR 
+THE ELSE BLOCK GETS EXECUTED AFTER FINISHING THE FOR LOOP
+'''
+list = ["tallent", "accurate", "ragul"] 
+for index in range(len(list)): 
+    print list[index] 
+else: 
+    print "the for loop has finished and now it is in else",
+'''
+OUTPUT:
+tallent
+accurate
+ragul
+the for loop has finished and now it is in else
+'''
