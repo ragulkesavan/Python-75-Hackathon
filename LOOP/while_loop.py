92f0576fa985f91cdec1064b5585844a63fcd762
ragulkesavan <srk7official1998@gmail.com>
2018-12-15 19:07:17 +0530

    ragul

---
+++ LOOP/while_loop.py
@@ -0,0 +1,23 @@
+#WHILE LOOP
+'''
+While loop is used to execute a block of statements repeatedly until a given a condition is satisfied. And when the condition becomes false, the line immediately after the loop in program is executed.
+'''
+#print first 5 numbers from 0
+i=0
+while i<5:
+  print i
+
+#indexing using loop variable i->print the elements in list
+l=[1,"accurate","torento",3]
+i=0
+while i<len(l):
+  print l[i]
+
+'''
+syntax:
+loop variable initialising
+while <condition>:
+  body_of_loop
+  re initialising loop variable
+
+'''
