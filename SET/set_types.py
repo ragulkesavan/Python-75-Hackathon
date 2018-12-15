594ccdfece3fec95570b79048cb7415c02a4e7cf
ragulkesavan <35163962+ragulkesavan@users.noreply.github.com>
2018-12-15 05:47:44 +0530

    Create set_types.py

---
+++ SET/set_types.py
@@ -0,0 +1,10 @@
+set1=set([1,2,3,"tallent","accurate"])
+set2=frozenset([4,5,6,"frozen","set"])
+print "normal set : ",set1
+print "frozen set : ",set2
+
+,,,
+OUTPUT:
+normal set :  set(['tallent', 1, 2, 3, 'accurate'])
+frozen set :  frozenset(['frozen', 'set', 4, 5, 6])
+,,,
